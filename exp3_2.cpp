8cd0b55c66667880b673c77a151ad23484d6c2b0
Vitesh Sethi <500076357@stu.upes.ac.in>
2020-06-23 21:26:07 +0530

    eight

---
+++ exp3_2.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+class Oxygen;
+class Hydrogen;
+class Sulphur;
+class Oxygen{
+int n;
+public:
+Oxygen(int n){
+this->n= n;
+}
+friend bool getChemicalCompound(Oxygen &, Hydrogen &, Sulphur &);
+};
+class Hydrogen{
+int n;
+public:
+Hydrogen(int n){
+this->n= n;
+}
+friend bool getChemicalCompound(Oxygen &, Hydrogen &, Sulphur &);
+};
+class Sulphur{
+int n;
+public:
+Sulphur(int n){
+this->n= n;
+}
+friend bool getChemicalCompound(Oxygen &, Hydrogen &, Sulphur &);
+};
+bool getChemicalCompound(Oxygen &o, Hydrogen &h, Sulphur &s){
+printf("Given Compound:\nH:%d\nS:%d\nO:%d\n\n", h.n, s.n, o.n);
+if(h.n==2 && s.n==1 && o.n==4){
+cout<<"Sulphuric Acid"<<endl;
+return true;
+}else if(h.n==2 && s.n==1 && o.n==3){
+cout<<"Sulphurous Acid"<<endl;
+return true;
+}
+else if(h.n==2 && s.n==3 && o.n==3)
+{
+cout<<"Thiosulphurous Acid"<<endl;
+return true;
+}
+else
+{
+cout<<"Given Compound was not found."<<endl;
+return false;
+}
+}
+int main(){
+Oxygen o(4);
+Hydrogen h(2);
+Sulphur s(1);
+getChemicalCompound(o, h, s);
+// (more) Tests
+Oxygen a(3);
+Hydrogen b(2);
+Sulphur c(1);
+getChemicalCompound(a, b, c);
+Oxygen l(4);
+Hydrogen m(2);
+Sulphur n(2);
+getChemicalCompound(l, m, n);
+Oxygen x(3);
+Hydrogen y(2);
+Sulphur z(3);
+getChemicalCompound(x, y, z);
+return 0;
+}
